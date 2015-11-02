e512ba928e3e6edb18828442150ebb7a5e0fc22c
Mohammad Amin Sameti <mamins1376@gmail.com>
2015-11-02 21:55:38 +0330

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MPDroid-CoverServer
-Python application for serving covers used by MPDroid l, using Flask microframework
+Python application for serving covers used by [MPDroid](https://github.com/abarisain/dmix), using [Flask](http://flask.pocoo.org/) microframework. files' metadata extracted using [mutagen](https://bitbucket.org/lazka/mutagen)
+
+# Installation
+Install Flask and mutagen with:
+$ sudo pip3 install flask mutagen
+
+Clone repo:
+$ git clone https://github.com/mamins1376/MPDroid-CoverServer.git
+
+Run it. by default, MPDroid uses port 80 on your machine, which needs superuser access level:
+$ sudo ./server.py <MUSICS DIRECTORY>
+
+it may take some minutes, depending on count of your music files.
---
+++ server.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+
+from flask import Flask,make_response,abort
+from mutagen.easyid3 import EasyID3
+from mutagen import File
+import sqlite3 as sqlite
+import os, sys
+app = Flask(__name__)
+
+# Set musics directory
+def error():
+  print('USAGE: server.py <MUSIC>\n  <MUSIC> must be the absolute path of you musics folder.\n')
+  exit()
+  
+if len(sys.argv) != 2:
+  error()
+
+MUSIC_DIR = str(sys.argv[-1])
+if not os.path.exists(MUSIC_DIR):
+  print('{} doesn\'t exist.\n'.format(MUSIC_DIR))
+  error()
+
+### CONFIGURATIONS ###
+# Database file used for indexing songs
+DB_FILE = os.path.abspath('songs.db')
+
+# The cover file that used when no cover is available.
+# Replace with False to disable it
+NO_COVER = os.path.abspath('no_cover.jpg')
+
+# Host ip or name. you often don't need to change it
+HOST = '0.0.0.0'
+
+# Note: for port <= 1024, you need superuser access.
+PORT = 80
+
+# app debug. disable it for releases.
+app.debug = True
+
+
+class Database:
+
+  def __init__(self):
+    self.open()
+
+  def open(self):
+    self.conn = sqlite.connect(DB_FILE)
+    self.cur = self.conn.cursor()
+
+  def create_table(self):
+    self.cur.executescript('''
+        DROP TABLE IF EXISTS Songs;
+        CREATE TABLE Songs(Artist TEXT, Album TEXT, Title TEXT, Path TEXT);
+        ''')
+    self.conn.commit()
+
+  def insert(self,song):
+    query = 'INSERT INTO Songs VALUES(?, ?, ?, ?)'
+    self.cur.execute(query,
+        (
+          song.artist,
+          song.album,
+          song.title,
+          song.path,
+          ),
+        )
+    self.conn.commit()
+
+  def get_song(self, artist, album, title='any'):
+    if title == 'any':
+      query = 'SELECT Path FROM Songs WHERE Artist=? AND Album=?'
+      self.cur.execute(query,(artist,album))
+    else:
+      query = 'SELECT Path FROM Songs WHERE Artist=? AND Album=? AND Title=?'
+      self.cur.execute(query,(artist,album,title))
+  
+    self.conn.commit()
+    path = self.cur.fetchone()
+    return Song(artist,album,title,path)
+
+  def close(self):
+    self.conn.close()
+
+class Song:
+
+  def clean_arg(string):
+    string = str(string)
+    if string.endswith(']'):
+      return string[2:-2]
+    elif string.endswith(')'):
+      return string[2:-3]
+    else:
+      return string
+
+  def __init__(self, artist, album, title, path):
+    self.artist = Song.clean_arg(artist)
+    self.album = Song.clean_arg(album)
+    self.title = Song.clean_arg(title)
+    self.path = Song.clean_arg(path)
+
+  def __str__(self):
+    return '{} by {} from {}'.format(self.title, self.artist, self.album)
+
+
+
+def song_from_path(path):
+  song = EasyID3(path)
+  try:
+    artist = song['artist']
+  except:
+    artist = ''
+
+  try:
+    album  = song['album']
+  except:
+    album = ''
+
+  try:
+    title = song['title']
+  except:
+    title = ''
+
+  return Song(artist=artist,
+      album=album,
+      title=title,
+      path=path)
+
+def update_database(db):
+  db.create_table()
+  for root, dirs, files in os.walk(MUSIC_DIR):
+    for file in files:
+      if file.endswith(".mp3"):
+        path = os.path.join(root, file)
+        song = song_from_path(path)
+        db.insert(song)
+
+def init_database():
+  if not os.path.isfile(DB_FILE):
+    db = Database()
+    print('Indexing musics. Please wait...')
+    try:
+      update_database(db)
+    finally:
+      db.close()
+
+def get_cover_art(song):
+  if song.path == 'None':
+    return None
+  file = File(song.path)
+  APIC = None
+  for key in file.keys():
+    if 'APIC:' in key:
+      APIC = key
+  if APIC is None:
+    return None
+  artwork = file.tags[APIC].data
+  return artwork
+     
+def generate_response(Artist, Album, Title='any'):
+  db = Database()
+  song = db.get_song(Artist, Album, Title)
+  db.close()
+  cover_raw = get_cover_art(song)
+  if cover_raw is None:
+    if NO_COVER == False:
+      abort(404)
+    else:
+      with open(NO_COVER, 'rb') as f:
+        cover_raw = f.read()
+  resp = make_response(cover_raw)
+  resp.content_type = "image/jpeg"
+  return resp
+
+def is_title(title):
+  return not title in ['cover','folder','%placeholder_filename']
+
+
+
+@app.route("/<Artist>/<Album>/Covers/front.jpg")
+def album_cover(Artist, Album):
+  return generate_response(Artist, Album)
+
+@app.route("/<Artist>/<Album>/artwork/front.jpg")
+def album_artwork(Artist, Album):
+  return generate_response(Artist, Album)
+
+@app.route("/<Artist>/<Album>/<Title>.jpg")
+def song_cover_jpg(Artist, Album, Title):
+  if is_title(Title):
+    return generate_response(Artist, Album, Title)
+  else:
+    return generate_response(Artist, Album)
+
+
+init_database()
+
+try:
+  app.run(host=HOST,port=PORT)
+except PermissionError:
+  print('ERROR: can\'t open port {}: you have not enough premissions.'.format(PORT))
+  exit()
